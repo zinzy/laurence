b5a1d240f0b1e0dddec3bce4c2ede2cc61d8c0dc
Zinzy Waleson Geene <zinzygeene@gmail.com>
2026-08-19 13:34:57 +0200

    Removed Mastodon and Jekyll settings

--- indiekit.config.js
+++ indiekit.config.js
@@ -6,15 +6,8 @@ dotenv.config();
 const config = {
   /**
    * Plug-ins
-   *
-   * In this example we have chosen to use a publication preset for Jekyll,
-   * save posts and media files to a GitHub repository, and added a syndicator
-   * to share posts on a Mastodon server.
-   *
-   * See: https://getindiekit.com/configuration/#plugins
    */
   plugins: [
-    "@indiekit/preset-jekyll",
     '@indiekit/preset-hugo', 
     "@indiekit/store-github",
     "@indiekit/syndicator-mastodon",
@@ -22,21 +15,33 @@ const config = {
 
   /**
    * Publication options
-   *
-   * These values are used to configure application behaviour.
-   *
-   * See: https://getindiekit.com/configuration/#publication
    */
   publication: {
     me: process.env.PUBLICATION_URL,
+    
+    // Hugo-specific paths and URLs must be defined here so Indiekit uses them
+    postTypes: {
+      article: {
+        path: 'content/posts/{yyyy}-{MM}-{dd}-{slug}.md',
+        url: '{yyyy}/{MM}/{dd}/{slug}/',
+      },
+      note: {
+        path: 'content/micro/{yyyy}-{MM}-{dd}-{HH}-{mm}-{ss}-{slug}.md',
+        url: 'notes/{yyyy}/{MM}/{dd}/{HH}/{mm}/{ss}/',
+      },
+      like: {
+        path: 'content/likes/{yyyy}-{MM}-{dd}-{HH}-{mm}-{ss}-{slug}.md',
+        url: 'likes/{yyyy}/{MM}/{dd}/{HH}/{mm}/{ss}/',
+      },
+      photo: {
+        path: 'content/photos/{yyyy}-{MM}-{dd}-{HH}-{mm}-{ss}-{slug}.md',
+        url: 'photos/{yyyy}/{MM}/{dd}/{HH}/{mm}/{ss}/',
+      }
+    }
   },
 
   /**
    * GitHub content store options
-   *
-   * Other content stores are available.
-   *
-   * See: https://getindiekit.com/plugins/stores
    */
   "@indiekit/store-github": {
     user: process.env.GITHUB_USER,
@@ -44,40 +49,11 @@ const config = {
     branch: process.env.GITHUB_BRANCH,
   },
 
-  '@indiekit/preset-hugo': {
-    // This tells Indiekit exactly where to put your files
-    postTypes: {
-      article: {
-        path: 'content/posts/{yyyy}-{MM}-{dd}-{slug}.md',
-        url: '{yyyy}/{MM}/{dd}/{slug}.md',
-      },
-      note: {
-        path: 'content/micro/{yyyy}-{MM}-{dd}-{HH}-{mm}-{ss}-{slug}.md',
-        url: 'notes/{yyyy}/{MM}/{dd}/{HH}/{mm}/{ss}.md',
-      },
-      like: {
-        path: 'content/likes/{yyyy}-{MM}-{dd}-{HH}-{mm}-{ss}-{slug}.md',
-        url: 'likes/{yyyy}/{MM}/{dd}/{HH}/{mm}/{ss}.md',
-      },
-      photo: {
-        path: 'content/photos/{yyyy}-{MM}-{dd}-{HH}-{mm}-{ss}-{slug}.md',
-        url: 'photos/{yyyy}/{MM}/{dd}/{HH}/{mm}/{ss}.md',
-      }
-    }
-  },
-
-
   /**
-   * Mastodon syndicator options
-   *
-   * Multiple syndicators can be added.
-   *
-   * See: https://getindiekit.com/plugins/syndicators
+   * Preset Hugo options (front matter format, etc.)
    */
-  "@indiekit/syndicator-mastodon": {
-    checked: true,
-    url: process.env.MASTODON_URL,
-    user: process.env.MASTODON_USER,
+  '@indiekit/preset-hugo': {
+    frontMatterFormat: 'yaml', // or 'json' / 'toml' depending on your Hugo setup
   }
 };
 
